04a6e36b25b0b0d4c71778e4aecfa1fec5270d84
Asmaa-Alotaibi <asmaa-otaibi@yahoo.com>
2020-12-05 20:53:03 +0300

    done up to note model

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ app.use((req, res, next) => {
 //all errors (error handle middle ware)
 app.use((err, req, res, next) => {
   console.log(err);
-  res.status(err.status ?? 500);
-  res.json({ message: err.message ?? "internal server error" });
+  res.status(err.status || 500);
+  res.json({ message: err.message || "internal server error" });
 });
 const jj = express();
 app.get("/", (req, res) => {
